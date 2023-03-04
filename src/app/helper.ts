39d1caa7bbb3d7b293830daa3cbebfcd0d1ed5f9
Omar Arvelo <omararvelo81@gmail.com>
2023-03-04 00:22:19 -0400

    Updating Connection to Railway

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -2,6 +2,6 @@
 
 //let baseUrl = 'http://localhost:8080';  // DEV MODE
 
-let baseUrl = 'https://thevolkner-backend.up.railway.app' // PROD MODE
+let baseUrl = 'https://thevolkner-backend.up.railway.app' // PROD MODE FOR RAILWAY
 
 export default baseUrl;
